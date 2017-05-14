d79860b5f4b87bcbdd9169a5949e6019be4d3ce2
Natasha Hockey <1hockn57@solent.ac.uk>
2017-05-14 19:45:25 +0100

    Created design preview nav

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 $('.down-arrow').on('click', function(){
     
     //this remembers what's been clicked
+    //toggle the open class on and off every click
     $(this).toggleClass('open');
     
+    //if this has the class open
     if ($(this).hasClass('open') ){
         
+        //the image is an up arrow
         $(this).attr('src','images/Up-arrow.svg');    
         
     } else {
     
+        //else the image is a down arrow
         $(this).attr('src','images/Down-arrow.svg');
     
     }
     
+    //slide the menu up or down
     $('.sub-menu').slideToggle();
 
 });
